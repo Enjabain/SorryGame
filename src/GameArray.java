f296d269333d4aae94e4ad5255762bdb81b7db89
Christopher Morse <dignanmorse@gmail.com>
2012-03-24 02:51:47 -0400

    Added the Sorry!.html and sorry.css files to the project and enabled web service functionalities within the project. Added two small conditionals to the javascript in Sorry!.html to elimate unnecessary funtion calls and 'null' exceptions in the debug. Also, was able to eliminate a bug that would cause the player movement functionality to not work properly if a blank square was clicked before selecting the square containing the player you wish to move.

--- src/GameArray.java
+++ src/GameArray.java
@@ -2,4 +2,23 @@
  * CS Program: GameArray Programmer: Christopher Morse Date: 3/23/12
  */
 public class GameArray {
+    private PlayableSquare[] gameArray, redHomeArray, greenHomeArray, blueHomeArray, yellowHomeArray, startArrays;
+
+    public GameArray() {
+        intializeGameArrays();
+    }
+
+    private void intializeGameArrays() {
+        gameArray = new PlayableSquare[60];
+        redHomeArray = new PlayableSquare[6];
+        greenHomeArray = new PlayableSquare[6];
+        blueHomeArray = new PlayableSquare[6];
+        yellowHomeArray = new PlayableSquare[6];
+        startArrays = new PlayableSquare[4];
+    }
+
+    public PlayableSquare[] getGameArray() {
+        return gameArray;
+    }
+
 }
