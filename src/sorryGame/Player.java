2317b3c82475a8c6aae914102ace95eb0fece34e
Ebrahim Kobeissi <eebs@eebs-tech.com>
2012-03-31 15:22:40 -0400

    added setters/getters

--- src/sorryGame/Player.java
+++ src/sorryGame/Player.java
@@ -1,5 +1,25 @@
 package sorryGame;
 
-public class Player {
+import java.awt.Color;
 
+abstract public class Player {
+
+	private String name;
+	private Color color;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
 }
