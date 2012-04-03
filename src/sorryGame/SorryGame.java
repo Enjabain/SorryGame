3353b1153983ad951b6c560f39752636f729ad76
Christopher Morse <dignanmorse@gmail.com>
2012-04-03 10:34:47 -0400

    Pre-Merge with Al Moataz changes.

--- src/sorryGame/SorryGame.java
+++ src/sorryGame/SorryGame.java
@@ -1,12 +1,18 @@
 package sorryGame;
 
-/**
- * CS Program: sorryGame.SorryGame Programmer: Christopher Morse Date: 3/23/12
- */
 public class SorryGame {
-    GameBoard gameBoard;
-
-    public boolean validateMove(String pieceID, String fromPosition, String toPosition) {
-        return false;
-    }
+	public static void main(String[] args){
+		GameBoard game = new GameBoard();
+		Deck deck = new Deck();
+		game.print();
+		int card = deck.getCard().getRank();
+		System.out.println(card);
+		if (game.validateMove(card, "g1", 57, 2, 1, 60)){
+            System.out.println("Move is Valid\n");
+            game.makeMove(card, "g1", 57, 2, 1, 60);
+		}
+		else
+            System.out.println("Move is Invalid\n");
+		game.print();
+	}
 }
