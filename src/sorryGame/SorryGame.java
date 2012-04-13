6204517a00de1f3757b0275f36d346524298b67a
Moataz <Moataz@10.245.6.134>
2012-04-13 10:32:37 -0400

    Bugs in ValidateMove() method fixed.

--- src/sorryGame/SorryGame.java
+++ src/sorryGame/SorryGame.java
@@ -6,29 +6,27 @@ public class SorryGame {
     Deck.Card currentCard;
 
     public SorryGame(){
-        GameBoard board = new GameBoard();
-        Deck deck = new Deck();
-
+        board = new GameBoard();
+        deck = new Deck();
     }
 
     public boolean validateMove(String pieceID, int fromPosition, int toPosition, int fromPosition2, int toPosition2) {
         if (currentCard.getRank() == 7){
             return board.validateMove(currentCard.getRank(), pieceID, fromPosition, toPosition, fromPosition2, toPosition2);
         }
-        else{ return board.validateMove(currentCard.getRank(), pieceID, fromPosition, toPosition, 0, 0);}
-
+        else{ 
+        	return board.validateMove(currentCard.getRank(), pieceID, fromPosition, toPosition, 0, 0);
+        }
     }
 
     public static void main(String[] args){
-		//GameBoard game = new GameBoard();
         SorryGame game = new SorryGame();
-		//GameBoard board = game.board;
-        board.print();
+        game.board.print();
 		int card = game.deck.getCard().getRank();
 		System.out.println(card);
-		if (game.board.validateMove(4, "g1", 64, 65, 0, 0)){
+		if (game.board.validateMove(7, "g1", 58, 62, 0, 0)){
             System.out.println("Move is Valid\n");
-            game.board.makeMove(4, "g1", 64, 65, 0, 0);
+            game.board.makeMove(7, "g1", 58, 62, 0, 0);
 		}
 		else
             System.out.println("Move is Invalid\n");
