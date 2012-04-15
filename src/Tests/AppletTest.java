284e73138aad505990239e7b549f16e5be5c0d76
Christopher Morse <dignanmorse@gmail.com>
2012-04-15 12:39:00 -0400

    Remove main method from GameApplet and place in separate test class.
    - removed redundant copies of DeckTest and Test directory.
    - Fix bugs in GameApplet when updating board and validating move.
    
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

---
+++ src/Tests/AppletTest.java
@@ -0,0 +1,40 @@
+import sorryGame.GameApplet;
+
+/**
+ * CS Program: AppletTest Programmer: Christopher Morse Date: 4/15/12
+ */
+public class AppletTest {
+    public static void main(String[] args) throws Exception {
+        GameApplet testApplet = new GameApplet();
+        //                for(Integer i =0; i<10; i++){
+        //                    System.out.println(testApplet.drawCard());
+        //                }
+        int crd = testApplet.drawCard();
+        while(crd != 1){
+            crd = testApplet.drawCard();
+            System.out.println(crd);
+        }
+        boolean valid = testApplet.validateMove("y0", "yellowstart", "49");
+        System.out.println("yellow move is valid: " + valid);
+        valid = testApplet.validateMove("r0", "redstart", "19");
+        System.out.println("red move is valid: " + valid);
+        while(crd != 10){
+            crd = testApplet.drawCard();
+            System.out.println(crd);
+        }
+        for (int i=49;i<99; i=i+10) {
+            int init = i % 60;
+            int fin = (init + 10) % 60;
+            String initialPos = Integer.toString(init);
+            String finalPos = Integer.toString(fin);
+            valid = testApplet.validateMove("y0", initialPos, finalPos);
+            System.out.println(valid);
+        }
+        valid = testApplet.validateMove("y0", "39", "79");
+        System.out.println("yellow move is valid: " + valid);
+
+
+        //System.out.println(valid1);
+        //        testApplet.setGame(new SorryGame());
+    }
+}
--- src/Tests/DeckTest.java
+++ src/Tests/DeckTest.java
@@ -1,5 +1,3 @@
-package Tests;
-
 import sorryGame.Deck;
 
 /**
