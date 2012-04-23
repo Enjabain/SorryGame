3d27613c759189a7f4f52e01db7251cbb5f02945
Christopher Morse <dignanmorse@gmail.com>
2012-04-22 21:02:48 -0400

    Fix start position id's to match actual position id's of GameBoard.
    Signed-off-by: Christopher Morse <dignanmorse@gmail.com>

--- src/Tests/AppletTest.java
+++ src/Tests/AppletTest.java
@@ -1,13 +1,16 @@
-package Tests;
+/*
 
 import sorryGame.GameApplet;
 
+*/
 /**
  * CS Program: AppletTest Programmer: Christopher Morse Date: 4/15/12
- */
+ *//*
+
 public class AppletTest {
     public static void main(String[] args) throws Exception {
         GameApplet testApplet = new GameApplet();
+//        testApplet.startGame("morsecp", "red", "easy");
         //                for(Integer i =0; i<10; i++){
         //                    System.out.println(testApplet.drawCard());
         //                }
@@ -16,10 +19,10 @@ public class AppletTest {
             crd = testApplet.drawCard();
             System.out.println(crd);
         }
-        boolean valid = testApplet.validateMove("y0", "yellowstart", "49");
-        System.out.println("yellow move is valid: " + valid);
-        valid = testApplet.validateMove("r0", "redstart", "19");
-        System.out.println("red move is valid: " + valid);
+        testApplet.validateMove("y0", "ystart", "49");
+        System.out.println("yellow move is valid: " + testApplet.valid);
+        testApplet.validateMove("r0", "rstart", "19");
+        System.out.println("red move is valid: " + testApplet.valid);
         while(crd != 10){
             crd = testApplet.drawCard();
             System.out.println(crd);
@@ -29,14 +32,15 @@ public class AppletTest {
             int fin = (init + 10) % 60;
             String initialPos = Integer.toString(init);
             String finalPos = Integer.toString(fin);
-            valid = testApplet.validateMove("y0", initialPos, finalPos);
-            System.out.println(valid);
+            testApplet.validateMove("y0", initialPos, finalPos);
+            System.out.println(testApplet.valid);
         }
-        valid = testApplet.validateMove("y0", "39", "79");
-        System.out.println("yellow move is valid: " + valid);
+        testApplet.validateMove("y0", "39", "79");
+        System.out.println("yellow move is valid: " + testApplet.valid);
 
 
         //System.out.println(valid1);
         //        testApplet.setGame(new SorryGame());
     }
 }
+*/
